48dcc149fc12727ac1a40073408454716cb37b7a
erudite101 <71517768+erudite101@users.noreply.github.com>
2022-05-10 15:20:17 -0700

    tuesdayupdate
    :)

---
+++ CameraPlasmaGasProject/src/AlgorithmForMachineLearning/stockRandomizer.java
@@ -0,0 +1,22 @@
+package AlgorithmForMachineLearning;
+
+public class stockRandomizer {
+
+	public static void stock1Randomizer() {
+			
+		String[] stock1List = {}; 
+				
+		String picker1 = "AAPL";
+		
+	}
+	
+	public static void stock2Randomizer() {
+		
+		String[] stock2List = {};
+		
+		
+		String picker2 = "TSLA";
+	}
+	
+	
+}
