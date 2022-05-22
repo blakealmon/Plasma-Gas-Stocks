ef39fe4def15aab205abe5aa5e511dac479daa94
erudite101 <71517768+erudite101@users.noreply.github.com>
2022-05-22 14:24:58 -0700

    Revert "cleaning"
    This reverts commit 2b26cad211ca9fb0d725d237d3c0397bdddc6aaf.

---
+++ CameraPlasmaGasProject/src/AlgorithmForMachineLearning/stockRandomizer.java
@@ -0,0 +1,26 @@
+package AlgorithmForMachineLearning;
+
+public class stockRandomizer {
+	boolean Smooth = false;
+	boolean Cold = false;
+	boolean Round = false;
+	public static void stock1Randomizer() {
+			
+		String[] stock1List = {}; 
+				
+		String picker1 = "AAPL";
+		
+	}
+	
+	public static void stock2Randomizer() {
+		
+		String[] stock2List = {};
+		
+		
+		String picker2 = "TSLA";
+	}
+	
+	
+	
+	
+}
